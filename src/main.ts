987c4eac2e262a8707e17d7655c44143fa4d6efc
Vinícius Diller <viniciusdiller@hotmail.com>
2026-07-31 12:31:32 -0300

    dnv tentnado

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from "@nestjs/common";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
-import { json, urlencoded } from "express";
+import * as express from "express";
 import { join } from "path";
 
 async function bootstrap() {
@@ -12,8 +12,11 @@ async function bootstrap() {
 
   app.setGlobalPrefix("api");
 
-  app.use(json({ limit: "10mb" }));
-  app.use(urlencoded({ limit: "10mb", extended: true }));
+  app.use("/api/uploads", express.static(join(__dirname, "..", "uploads")));
+  app.use("/api/public", express.static(join(__dirname, "..", "public")));
+
+  app.use(express.json({ limit: "10mb" }));
+  app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
   app.enableCors({
     origin: (origin, callback) => callback(null, origin ?? true),
@@ -31,14 +34,6 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-
-  app.useStaticAssets(join(__dirname, "..", "uploads"), {
-    prefix: "/api/uploads/",
-  });
-
-  app.useStaticAssets(join(__dirname, "..", "public"), {
-    prefix: "/api/public/",
-  });
 
   const config = new DocumentBuilder()
     .setTitle("Apaixone-Se API")
